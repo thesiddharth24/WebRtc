fbc3b2b5e8dec79f0776307a19656c0e142fe625
thesiddharth24 <siddharth20it028@gmail.com>
2023-12-17 23:32:56 +0530

    first commit

---
+++ Server/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const { Server, Socket } = require('socket.io')
+const bodyParser = require('body-parser')
+
+
+const io = new Server({
+    cors: true,
+});
+const app = express();
+
+
+app.use(bodyParser.json());
+
+const emailToSocketMapping = new Map();
+const socketToEmailMapping = new Map();
+
+io.on('connection' , socket => {
+    //yaha signeling ka code likho 
+    console.log("New Connection");
+    socket.on('join-room' , data => {
+        const { roomId , emailId } = data;
+        console.log("User " , emailId , " joined " , roomId);
+        emailToSocketMapping.set(emailId , socket.id);
+        socketToEmailMapping.set(socket.id , emailId);
+        socket.join(roomId);
+        socket.emit('joined-room' , { roomId });
+        socket.broadcast.to(roomId).emit("user-joined", { emailId });
+    });
+
+    socket.on('call-user' , data => {
+        const { emailId , offer } = data;
+        const fromEmail = socketToEmailMapping.get(socket.id);
+        const socketId = emailToSocketMapping.get(emailId);
+        socket.to(socketId).emit('incomming-call', { from: fromEmail , offer}) // incomming call aa rahi hai from this email nd ye uski offer hai 
+    })
+})
+
+
+app.listen(8000 , () => {
+    console.log(`App is listening on PORT : 8000`)
+})
+io.listen(8001);
